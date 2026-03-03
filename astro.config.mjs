89d5dc15e2fd9284ae4db9865096abfbc8dce306
kevinamayatech-prog <kevinamayatech@gmail.com>
2026-03-02 22:42:24 -0400

    Update astro.config.mjs
    Nueva correcion con datos originales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // O las integraciones que tengas
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  // AGREGAR ESTA SECCIÓN:
+  site: "https://opticabellezavision.com",
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    icon()
+  ],
   server: {
     host: true,
     port: 4321
   }
 });
-
